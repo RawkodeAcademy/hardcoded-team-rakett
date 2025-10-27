889bf1e2a363702e7d21383ead24b06525f92255
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2025-10-27 10:28:23 +0100

    legge til tokenizer i aggregator

--- Hardcoded/DotnetAggregator/Program.cs
+++ Hardcoded/DotnetAggregator/Program.cs
@@ -9,17 +9,21 @@ var app = builder.Build();
 
 app.MapPost("/analyze", async ([FromBody] AggregatorRequest request) =>
 {
+    var requestString = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+    
     using var client = new HttpClient();
-
-    var normalizerResponse = await client.PostAsync("http://normalizer:8080",
-        new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json"));
-
+    var normalizerResponse = await client.PostAsync("http://normalizer:8080", requestString);
     var normalizerContent = await normalizerResponse.Content.ReadAsStringAsync();
     var normalized = JsonSerializer.Deserialize<ValueResponse>(normalizerContent)?.Value;
 
+    var tokenizerResponse = await client.PostAsync("http://tokenizer:8080", requestString);
+    var tokenizerContent = await tokenizerResponse.Content.ReadAsStringAsync();
+    var tokens = JsonSerializer.Deserialize<ValueResponse>(tokenizerContent)?.Value;
+
     return new AggregatorResponse
     {
-        Normalized = normalized
+        Normalized = normalized,
+        Tokens = tokens
     };
 });
 
@@ -41,4 +45,7 @@ public class AggregatorResponse
 {
     [JsonPropertyName("normalized")]
     public string Normalized { get; set; }
+    
+    [JsonPropertyName("tokens")]
+    public string Tokens { get; set; }
 }
